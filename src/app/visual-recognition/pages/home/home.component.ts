d8c3d008708005aa13b538c3eb8b0d28cc9a74a3
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-08-13 15:29:43 -0500

    uso de boton para habilitar el uso del compoente de camara

--- src/app/visual-recognition/pages/home/home.component.ts
+++ src/app/visual-recognition/pages/home/home.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-
+  statusComponent: boolean = false;
+  openComponent() {
+    this.statusComponent = !this.statusComponent;
+  }
 }
